19548567495c08334b2e46e7afd7d6273556fbd5
Jason Lang <52687680+jdenalil@users.noreply.github.com>
2021-02-04 19:09:49 -0500

    Update sign_recognition.py

--- Autonomy/sign_recognition/sign_recognition.py
+++ Autonomy/sign_recognition/sign_recognition.py
@@ -104,8 +104,8 @@ class SignRecognition:
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--image_folder", type=str, default="data/samples", help="path to dataset")
-    parser.add_argument("--model_def", type=str, default="config/yolov3.cfg", help="path to model definition file")
-    parser.add_argument("--weights_path", type=str, default="weights/yolov3.weights", help="path to weights file")
+    parser.add_argument("--model_def", type=str, default="yolov3-LISA.cfg", help="path to model definition file")
+    parser.add_argument("--weights_path", type=str, default="yolov3.weights", help="path to weights file")
     parser.add_argument("--class_path", type=str, default="data/coco.names", help="path to class label file")
     parser.add_argument("--conf_thres", type=float, default=0.8, help="object confidence threshold")
     parser.add_argument("--nms_thres", type=float, default=0.4, help="iou threshold for non-maximum suppression")
